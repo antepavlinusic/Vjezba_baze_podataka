ab976828625958bc36b800c0fbbd3ad843d735a3
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 12:37:27 +0200

    Muzej
    Baza podataka muzej

---
+++ muzej.sql
@@ -0,0 +1,36 @@
+# Linija koja pocne s hash je komentar
+# Ovo je SQL skripta
+# naredba za izvodjenje 
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba\muzej.sql
+
+drop database if exists muzej;
+create database muzej;
+use muzej;
+
+create table izlozba(
+    naziv           varchar(50),
+    datumpocetka    decimal(50),
+    vrijemepocetka  decimal(50),
+    datumzavrsetka  decimal(50),
+    vrijednost      decimal(50),
+    organizator     varchar(50)
+);
+
+create table djela(
+    naziv           varchar(50),
+    metoda          varchar(50),
+    autor           varchar(50)
+);
+
+create table sponzor(
+    naziv           varchar(50),
+    fond            decimal(50)
+);
+
+create table kustos(
+    ime             varchar(50),
+    prezime         varchar(50),
+    oib             decimal(50),
+    iban            decimal(50),
+    izlozba         varchar(50)
+);
