65453a0f6550724f8b5eaf23b189ff2e13911309
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 15:45:57 +0200

    Update salon_za_uljepsavanje.sql

--- salon_za_uljepsavanje.sql
+++ salon_za_uljepsavanje.sql
@@ -8,32 +8,39 @@ create database salon_za_uljepsavanje;
 use salon_za_uljepsavanje;
 
 create table djelatnica(
-    oib                    decimal(50),
-    iban                   decimal(50),
-    ugovor                 char(11)
+    sifra                  int not null primary key auto_increment,
+    osoba                  int not null,
+    oib                    char(11),
+    iban                   varchar(50),
+    ugovor                 varchar(50)
 );
 
 create table osoba(
-    ime                    varchar(50),
-    prezime                varchar(50),
-    kontakt                char(11)
+    sifra                  int not null primary key auto_increment,
+    ime                    varchar(50) not null,
+    prezime                varchar(50) not null,
+    kontakt                varchar(50)
 );
 
 create table materijal(
-    ruz                    varchar(50),
-    raspica                varchar(50),
-    skarice                varchar(50),
-    lampe                  char(11)
+    sifra                  int not null primary key auto_increment,
+    ruz                    varchar(50) not null,
+    raspica                varchar(50) not null,
+    skarice                varchar(50) not null,
+    uvlampa                varchar(50) not null
 );
 
 create table usluga(
+    sifra                  int not null primary key auto_increment,
     sminkanje              varchar(50),
     pedikura               varchar(50),
     manikura               varchar(50),
-    materijal              char(11)
+    materijal              varchar(50) not null
 );
 
 create table korisnik(
-    vrijemenarucivanja     varchar(50),
-    usluga                 char(11)
+    sifra                  int not null primary key auto_increment,
+    osoba                  int not null,
+    vrijemenarucivanja     datetime,
+    usluga                 varchar (50) not null
 );
