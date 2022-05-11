59e11042a1e4f832851f409aac8a1ec6c3bd8039
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 12:36:20 +0200

    djecji_vrtiv
    Baza podataka za djecji vrtic

---
+++ djecji_vrtic.sql
@@ -0,0 +1,40 @@
+# Linija koja pocne s hash je komentar
+# Ovo je SQL skripta
+# naredba za izvodjenje 
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\djecji_vrtic.sql
+
+drop database if exists djecji_vrtic; 
+create database djecji_vrtic;
+use djecji_vrtic;
+
+create table strucnasprema(
+    razina                  varchar(50),
+    obrazovnaustanova       varchar(50),
+    datumzavrsetka          varchar(50),
+    kod                     varchar(50)
+);
+
+create table odgajateljica(
+    iban                    varchar(50),
+    kontakt                 varchar(50),
+    strucnasprema           varchar(50)
+);
+
+create table osoba(
+    ime                     varchar(50),
+    prezime                 varchar(50),
+    oib                     varchar(50)
+);
+
+create table djeca(
+    imeroditelja            varchar(50),
+    prezimeroditelja        varchar(50),
+    kontaktroditelja        varchar(50)
+);
+
+create table odgojnaskupina(
+    naziv                   varchar(50),
+    makasimalnibrojdjece    varchar(50),
+    odgajateljica           varchar(50),
+    djeca varchar(50)
+);
