f8b938b5e15b07eb51f36f8aebda58ee29221d4e
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 15:45:42 +0200

    Update frizerski.sql

--- frizerski.sql
+++ frizerski.sql
@@ -8,25 +8,32 @@ create database frizerski;
 use frizerski;
 
 create table djelatnik(
-    oib                 decimal(50),
+    sifra               int not null primary key auto_increment,    
+    osoba               int not null,
+    oib                 char(11),
     iban                decimal(50),
-    ugovor              char(11)
+    ugovor              varchar(50)
 );
 
 create table usluga(
+    sifra               int not null primary key auto_increment,
+    cijena              decimal(18,2),
     bojanje             varchar(50),
     sisanje             varchar(50),
     brijanje            varchar(50),
-    pranje              char(11)   
+    pranje              varchar(50)  
 );
 
 create table osoba(
-    ime                 varchar(50),
-    prezime             varchar(50),
-    kontakt             char(11)
+    sifra               int not null primary key auto_increment,
+    ime                 varchar(50) not null,
+    prezime             varchar(50) not null,
+    kontakt             varchar(50)
 );
 
 create table korisnik(
-    vrijemenarucivanja  varchar(50),
-    usluga              char(11)
+    sifra               int not null primary key auto_increment,
+    osoba               int not null,
+    vrijemenarucivanja  datetime,
+    usluga              int not null
 );
