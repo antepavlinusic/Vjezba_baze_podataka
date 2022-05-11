08fa4f4b01711b2c2f8757d13888b8e849df4591
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 12:48:44 +0200

    frizerski
    Mijenjanje path-a

--- frizerski.sql
+++ frizerski.sql
@@ -1,7 +1,7 @@
 # Linija koja pocne s hash je komentar
 # Ovo je SQL skripta
 # naredba za izvodjenje 
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\frizerski.sql
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba_baze_podataka\frizerski.sql
 
 drop database if exists frizerski;
 create database frizerski;
