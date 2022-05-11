b59299ad6b734a0ae3714eaae8a96d950fb647fc
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 12:48:51 +0200

    muzej
    Mijenjanje path-a

--- muzej.sql
+++ muzej.sql
@@ -1,7 +1,7 @@
 # Linija koja pocne s hash je komentar
 # Ovo je SQL skripta
 # naredba za izvodjenje 
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba\muzej.sql
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba_baze_podataka\muzej.sql
 
 drop database if exists muzej;
 create database muzej;
