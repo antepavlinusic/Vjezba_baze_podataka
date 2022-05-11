bc9bf00dc566deaa87e4e1d3f9a3662de4aaf94b
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-11 15:46:13 +0200

    Update urar_silvija.sql

--- urar_silvija.sql
+++ urar_silvija.sql
@@ -8,33 +8,41 @@ create database urar_silvija;
 use urar_silvija;
 
 create table segrt(
+    sifra               int not null primary key auto_increment,
+    osoba               int not null,
     zadatak             varchar(50)
 );
 
 create table urar(
-    segrt               varchar(50)
+    sifra               int not null primary key auto_increment,         
+    osoba               int not null,
+    segrt               int not null
 );
 
 create table osoba(
-    ime                 varchar(50),
-    prezime             varchar(50),
-    oib                 decimal(50),
-    iban                decimal(50)
+    sifra               int not null primary key auto_increment,
+    ime                 varchar(50) not null,
+    prezime             varchar(50) not null,
+    oib                 char(11),
+    iban                varchar(50)
 );
 
 create table popravak(
-    statuspopravka      varchar(50),
-    cijena              decimal(50),
-    korisnik            varchar(50),
-    kodpopravka         decimal(50)
+    sifra               int not null primary key auto_increment,
+    statuspopravka      varchar(50) not null,
+    cijena              decimal(18,2),
+    korisnik            varchar(50) not null,
+    kodpopravka         varchar(50) not null
 );
 
 create table sat(
-    model               varchar(50),
-    materijal           varchar(50)
+    sifra               int not null primary key auto_increment,
+    model               varchar(50) not null,
+    materijal           varchar(50) not null
 );
 
 create table narudzba(
-    popravak            varchar(50),
-    sat                 varchar(50)
+    sifra               int not null primary key auto_increment,
+    popravak            int not null,
+    sat                 int not null
 );
