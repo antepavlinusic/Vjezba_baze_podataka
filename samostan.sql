0cd4bf1a7b47b4ffb821c416bf2cb8860397ba6c
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-12 11:39:41 +0200

    samostan kucanje ERA diagrama

--- samostan.sql
+++ samostan.sql
@@ -8,29 +8,41 @@ create database samostan;
 use samostan;
 
 create table svecenik(
-    sifra       int not null primary key auto_increment,
-    osoba       int not null,
+    sifra                   int not null primary key auto_increment,
+    posao                   int not null,
+    nadredjenisvecenik      int not null,
+    osoba                   int not null
 );
 
 create table nadredjenisvecenik(
-    sifra       int not null primary key auto_increment
-    osoba       int not null,
-    svecenik    varchar(50) not null
+    sifra                   int not null primary key auto_increment,
+    osoba                   int not null,
+    svecenik                int not null
 );
 
 create table osoba(
-    sifra       int not null primary key auto_increment,
-    ime         varchar(50) not null,
-    prezime     varchar(50)not null,
-    oib         char(11) not null,
-    iban        varchar(50),
-    titula      varchar(50)
+    sifra                   int not null primary key auto_increment,
+    ime                     varchar(50) not null,
+    prezime                 varchar(50)not null,
+    oib                     char(11) not null,
+    iban                    varchar(50),
+    titula                  varchar(50)
 );
 
 create table posao(
-    sifra       int not null primary key auto_increment,
-    svetamisa   varchar(50),
-    ispovjed    varchar(50),
-    krstenje    varchar(50),
-    zenidba     varchar(50)
+    sifra                   int not null primary key auto_increment,
+    svetamisa               varchar(50),
+    ispovjed                varchar(50),
+    krstenje                varchar(50),
+    zenidba                 varchar(50)
 );
+
+
+alter table  svecenik add foreign key (nadredjenisvecenik) references nadredjenisvecenik(sifra);
+
+alter table svecenik add foreign key (osoba) references osoba(sifra);
+
+alter table nadredjenisvecenik add foreign key (osoba) references osoba(sifra);
+
+alter table svecenik add foreign key (posao) references posao(sifra);
+
