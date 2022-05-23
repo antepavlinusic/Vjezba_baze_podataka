8753bfc9e9b9baef576b870cd091af2b02deaaf6
antepavlinusic <ante.pavlinusic@gmail.com>
2022-05-23 13:33:20 +0200

    taksi sluzba
    Postavljanje za prikaz dijakritičkih znakova

--- taksi_sluzba.sql
+++ taksi_sluzba.sql
@@ -1,7 +1,7 @@
 # Linija koja pocne s hash je komentar
 # Ovo je SQL skripta
 # naredba za izvodjenje 
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba_baze_podataka\taksi_sluzba.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Ante031\Documents\EdunovaPP25\Baze-podataka\Vjezba_baze_podataka\taksi_sluzba.sql
 
 drop database if exists taksi_sluzba;
 create database taksi_sluzba;
